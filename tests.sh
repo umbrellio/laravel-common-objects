e7ceb9f814279cfcb13a1cd9aa53393cfe047c18
Korben Dal <korben.dal@okwork.io>
2024-04-25 15:33:09 +0200

    remove tests

--- tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-COMPOSER_MEMORY_LIMIT=-1 composer update
-composer lint
-php -d pcov.directory='.' vendor/bin/phpunit --coverage-html build --coverage-text
-
--- tests/FunctionalTestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Umbrellio\Common\tests;
-
-abstract class FunctionalTestCase extends TestCase
-{
-}
